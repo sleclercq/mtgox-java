665e575ac6dac62e4c58b8b1779d5d946f7cb618
Grant Sparks <grant@sparks.to>
2012-12-31 17:16:38 +1000

    Documentation

--- src/main/java/to/sparks/mtgox/MtGoxAPI.java
+++ src/main/java/to/sparks/mtgox/MtGoxAPI.java
@@ -31,33 +31,124 @@ public interface MtGoxAPI {
         Bid, Ask
     }
 
+    /**
+     *
+     * @return The java currency used by this api connection.
+     */
     Currency getBaseCurrency();
 
+    /**
+     * Get the market full depth from the MtGox exchange
+     *
+     * @return An object containing the full market depth information
+     * @throws Exception
+     */
     FullDepth getFullDepth() throws Exception;
 
+    /**
+     * A provate function that requires credentials. Get all open orders.
+     *
+     * @return An array of open orders
+     * @throws Exception
+     */
     Order[] getOpenOrders() throws Exception;
 
+    /**
+     * Return the result of an order request
+     *
+     * @param orderType Bid or Ask
+     * @param orderRef The reference string that was returned when the order was
+     * pleaced
+     * @return
+     * @throws Exception
+     */
     OrderResult getOrderResult(OrderType orderType, String orderRef) throws Exception;
 
+    /**
+     * Get all depth events since a particular time (or whenever this api object
+     * was created)
+     *
+     * @param timestamp The java timestamp to return events since.
+     * @return
+     */
     List<Depth> getAllDepthSince(long timestamp);
 
+    /**
+     * Get all trade events since a particular time (or whenever this api object
+     * was created)
+     *
+     * @param timestamp The java timestamp to return trades since.
+     * @return
+     */
     List<Trade> getAllTradesSince(long timestamp);
 
+    /**
+     * Get all depth events since this api object was created
+     */
     List<Depth> getDepthHistory();
 
+    /**
+     * Get all ticker events since this api object was created
+     */
     List<Ticker> getTickerHistory();
 
+    /**
+     * Get all trade events since this api object was created
+     */
     List<Trade> getTradeHistory();
 
+    /**
+     * Request the current ticker from MtGox
+     *
+     * @return
+     * @throws Exception
+     */
     Ticker getTicker() throws Exception;
 
+    /**
+     * Place a bitcoin order at market price.
+     *
+     * @param orderType Bid or Ask
+     * @param volume The volume of bitcoins
+     * @return Order reference number
+     * @throws Exception
+     */
     String placeMarketOrder(OrderType orderType, MtGoxUnitOfCredit volume) throws Exception;
 
+    /**
+     * Place a bitcoin order at a particular price
+     *
+     * @param orderType Bid or Ask
+     * @param price The price in the base currency of this api instance
+     * @param volume The volume of bitcoins
+     * @return Order reference number
+     * @throws Exception
+     */
     String placeOrder(OrderType orderType, MtGoxUnitOfCredit price, MtGoxUnitOfCredit volume) throws Exception;
 
+    /**
+     * Get private information about the currently logged-in account
+     *
+     * @return
+     * @throws Exception
+     */
     AccountInfo getAccountInfo() throws Exception;
 
+    /**
+     * Get MtGox special information about a currency.
+     *
+     * @param currency The java currency
+     * @return
+     * @throws Exception
+     */
     CurrencyInfo getCurrencyInfo(Currency currency) throws Exception;
 
+    /**
+     * Get MtGox special information about a currency.
+     *
+     * @param currencyCode The currency code
+     * @return
+     * @throws Exception
+     */
     CurrencyInfo getCurrencyInfo(String currencyCode) throws Exception;
 }
