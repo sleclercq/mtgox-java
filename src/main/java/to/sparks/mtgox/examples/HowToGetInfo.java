42d6309bf9c8219d927808f1ad5b6bc018b45512
Grant Sparks <grant@sparks.to>
2013-01-17 00:29:27 +1000

    Added some examples

---
+++ src/main/java/to/sparks/mtgox/examples/HowToGetInfo.java
@@ -0,0 +1,62 @@
+/*
+ * The MtGox-Java API is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The MtGox-Java API is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public License
+ * along with the MtGox-Java API .  If not, see <http://www.gnu.org/licenses/>.
+ */
+package to.sparks.mtgox.examples;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.lang.ArrayUtils;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import to.sparks.mtgox.MtGoxAPI;
+import to.sparks.mtgox.model.AccountInfo;
+import to.sparks.mtgox.model.Order;
+import to.sparks.mtgox.model.Ticker;
+
+/**
+ * This example will show how to get some types of information from MtGox
+ *
+ * @author SparksG
+ */
+public class HowToGetInfo {
+
+    static final Logger logger = Logger.getLogger(HowToGetInfo.class.getName());
+
+    public static void main(String[] args) throws Exception {
+
+        // Obtain a $USD instance of the API
+        ApplicationContext context = new ClassPathXmlApplicationContext("to/sparks/Beans.xml");
+        MtGoxAPI mtgoxUSD = (MtGoxAPI) context.getBean("mtgoxUSD");
+
+        Ticker ticker = mtgoxUSD.getTicker();
+        logger.log(Level.INFO, "Last price: {0}", ticker.getLast().getDisplay());
+
+        // Get the private account info
+        AccountInfo info = mtgoxUSD.getAccountInfo();
+        logger.log(Level.INFO, "Logged into account: {0}", info.getLogin());
+
+        Order[] openOrders = mtgoxUSD.getOpenOrders();
+
+        if (ArrayUtils.isNotEmpty(openOrders)) {
+            for (Order order : openOrders) {
+                logger.log(Level.INFO, "Open order: {0} status: {1} price: {2}{3} amount: {4}", new Object[]{order.getOid(), order.getStatus(), order.getCurrency().getCurrencyCode(), order.getPrice().getDisplay(), order.getAmount().getDisplay()});
+            }
+        } else {
+            logger.info("There are no currently open bid or ask orders.");
+        }
+
+        // Shutdown the api when you are finished
+        mtgoxUSD.shutdown();
+    }
+}
---
+++ src/main/java/to/sparks/mtgox/examples/PlaceOrders.java
@@ -0,0 +1,60 @@
+/*
+ * The MtGox-Java API is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The MtGox-Java API is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public License
+ * along with the MtGox-Java API .  If not, see <http://www.gnu.org/licenses/>.
+ */
+package to.sparks.mtgox.examples;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import to.sparks.mtgox.MtGoxAPI;
+import to.sparks.mtgox.model.CurrencyInfo;
+import to.sparks.mtgox.model.MtGoxBitcoin;
+import to.sparks.mtgox.model.MtGoxFiatCurrency;
+
+/**
+ * Examples of ordering functionality.
+ * WARNING: Executing this code will attempt to place orders at the MtGox
+ * Bitcoin Exchange
+ *
+ * @author SparksG
+ */
+public class PlaceOrders {
+
+    static final Logger logger = Logger.getLogger(PlaceOrders.class.getName());
+
+    public static void main(String[] args) throws Exception {
+
+        // Obtain a $USD instance of the API
+        ApplicationContext context = new ClassPathXmlApplicationContext("to/sparks/Beans.xml");
+        MtGoxAPI mtgoxUSD = (MtGoxAPI) context.getBean("mtgoxUSD");
+
+        // Obtain information about the base currency of the API instance
+        CurrencyInfo currencyInfo = mtgoxUSD.getCurrencyInfo(mtgoxUSD.getBaseCurrency());
+        logger.log(Level.INFO, "Base currency: {0}", currencyInfo.getCurrency().getCurrencyCode());
+
+
+        // Purchase 1.0 bitcoins for USD$0.01
+        MtGoxFiatCurrency fiatUnit = new MtGoxFiatCurrency(0.01D, currencyInfo);  // We use the currencyInfo to be explicit about what this money represents
+        MtGoxBitcoin bitcoinUnit = new MtGoxBitcoin(1.0D);  // You should probably use BigDecimals instead of double types to represent money.
+        String orderRef = mtgoxUSD.placeOrder(MtGoxAPI.OrderType.Bid, fiatUnit, bitcoinUnit);
+        logger.log(Level.INFO, "orderRef: {0}", new Object[]{orderRef});
+
+        // Cancel the order
+        mtgoxUSD.cancelOrder(MtGoxAPI.OrderType.Bid, orderRef);
+
+        // Shutdown the api when you are finished
+        mtgoxUSD.shutdown();
+    }
+}
